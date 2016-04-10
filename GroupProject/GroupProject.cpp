e967056b6c710d2c45f1bd709a1353efba203a60
Morgan Wesemann <morgan@thinkdigitl.com>
2016-04-10 15:37:35 -0500

    init commit

--- GroupProject/GroupProject.cpp
+++ GroupProject/GroupProject.cpp
@@ -1,9 +1,41 @@
-//
-//  GroupProject.cpp
-//  GroupProject
-//
-//  Created by Morgan Wesemann on 4/10/16.
-//  Copyright © 2016 Morgan Wesemann. All rights reserved.
-//
+/*
+ * GroupProject.cpp
+ *
+ *  Created on: Mar 31, 2014
+ *  Modified on: Mar 31, 2014
+ *  Author: D. Booth
+ */
 
-#include <stdio.h>
+#include "GroupProject.h"
+
+//GroupProject Constructor
+
+GroupProject::GroupProject(GLUT_Plotter* g){
+    this->g = g;
+}
+
+
+//GroupProject Main Game Loop
+void GroupProject::Play(void){
+    
+    //Check for Keyboard Hit
+    while(g->kbhit()){
+        int k = g->getKey();
+        switch (k){
+            case 27: exit(1); //ESC key
+		              break;
+        }
+    }
+    
+    
+    //Check for mouse click
+    while(g->click()){
+        
+        Click c;
+        c = g->getClick();
+    }
+    
+    // Update screen - draw game
+    g->Draw();
+}
+
