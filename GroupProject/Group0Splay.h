ec93d076c5c46f7dde5b8f73e1097bbe67f2f206
AJKgithub <Adam_Kim@baylor.edu>
2016-04-15 20:59:46 -0500

    Update Group0Splay.h

--- GroupProject/Group0Splay.h
+++ GroupProject/Group0Splay.h
@@ -18,9 +18,9 @@ class SplayNode
 public:
 	friend class SplayTree<Base>;
 
-	/*ID=0 DONE*/SplayNode (const Base &d = Base(),SplayNode*p=NULL,SplayNode*l=NULL,SplayNode*r=NULL)
-	:data(d),parent(p),left(l),right(r){}
-	/*ID=1 DONE*/~SplayNode(){delete left;delete right;}
+	/*ID=0 DONE*/SplayNode(const Base &d = Base(), SplayNode*p = NULL, SplayNode*l = NULL, SplayNode*r = NULL)
+		:data(d), parent(p), left(l), right(r) {}
+	/*ID=1 DONE*/~SplayNode() { delete left;delete right; }
 
 	/*ID=2 DONE*/const SplayNode *minNode() const;
 	/*ID=2 DONE*/const SplayNode *maxNode() const;
@@ -39,32 +39,32 @@ protected:
 template <class Base>
 void SplayNode<Base>::verifySearchOrder() const
 {
-    if (left)
-    {
-        assert(left->maxNode()->data < data);
-        left->verifySearchOrder();
-    }
-    if (right)
-    {
-        assert(data < right->minNode()->data);
-        right->verifySearchOrder();
-    }
+	if (left)
+	{
+		assert(left->maxNode()->data < data);
+		left->verifySearchOrder();
+	}
+	if (right)
+	{
+		assert(data < right->minNode()->data);
+		right->verifySearchOrder();
+	}
 }
 
 template <class Base>
 void SplayNode<Base>::verifyParentProperty() const
 {
-    if (left!=NULL)
-    {
-        assert(left->parent == this);
-        left->verifySearchOrder();
-    }
+	if (left != NULL)
+	{
+		assert(left->parent == this);
+		left->verifySearchOrder();
+	}
 
-    if (right!=NULL)
-    {
-    	assert(right->parent == this);
-        right->verifySearchOrder();
-    }
+	if (right != NULL)
+	{
+		assert(right->parent == this);
+		right->verifySearchOrder();
+	}
 }
 
 ////////////////////////////////////////////////////////////////////////////////
@@ -76,7 +76,9 @@ class SplayTree
 {
 public:
 	/*ID=0 DONE*/SplayTree()
-	{root = NULL;}
+	{
+		root = NULL;
+	}
 	/*ID=1 DONE*/virtual ~SplayTree() { SplayNode<Base> *r = root; root = NULL; delete r; }
 
 	/*ID=2 */void insert(const Base &item);
@@ -84,10 +86,10 @@ public:
 	/*ID=4 */bool find(const Base &item);
 	/*ID=5 */void splayToRoot(SplayNode<Base>* ptr);
 	/*ID=6 DONE*/void printLevelOrder(ostream &os = cout) const;
-	/*ID=7 DONE*/void printPreorder(ostream &os = cout) const{if (root) root->printPreorder(os);}
+	/*ID=7 DONE*/void printPreorder(ostream &os = cout) const { if (root) root->printPreorder(os); }
 
-    void verifySearchOrder() const{ if (root) root->verifySearchOrder(); }
-    void verifyParentProperty() const{ if (root) root->verifyParentProperty(); }
+	void verifySearchOrder() const { if (root) root->verifySearchOrder(); }
+	void verifyParentProperty() const { if (root) root->verifyParentProperty(); }
 
 protected:
 	SplayNode<Base> *root;
