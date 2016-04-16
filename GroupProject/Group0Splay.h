80a2abf73dd32a2f47dd8cf5480f514ab4815e18
AJKgithub <Adam_Kim@baylor.edu>
2016-04-16 14:55:15 -0500

    Update Group0Splay.h

--- GroupProject/Group0Splay.h
+++ GroupProject/Group0Splay.h
@@ -5,8 +5,8 @@
 #include <iostream>
 #include <string>
 #include <cassert>
-
 using namespace std;
+
 template <class Base>
 class SplayTree;
 
@@ -56,14 +56,14 @@ void SplayNode<Base>::verifyParentProperty() const
 {
     if (left!=NULL)
     {
-        assert(left->parent == this);
-        left->verifySearchOrder();
+        assert(this->left->parent == this);
+        left->verifyParentProperty();
     }
 
     if (right!=NULL)
     {
-    	assert(right->parent == this);
-        right->verifySearchOrder();
+    	assert(this->right->parent == this);
+        right->verifyParentProperty();
     }
 }
 
@@ -75,28 +75,21 @@ template <class Base>
 class SplayTree
 {
 public:
-	/*ID=0 DONE*/SplayTree()
-	{root = NULL;}
-	/*ID=1 DONE*/virtual ~SplayTree() { SplayNode<Base> *r = root; root = NULL; delete r; }
+	/*DONE HERE*/SplayTree(){root = NULL;}
+	/*DONE HERE*/virtual ~SplayTree() { SplayNode<Base> *r = root; root = NULL; delete r; }
+	/*DONE HERE*/void print(ostream &os = cout) {printPreorder(os);}
+	/*DONE HERE*/void verifySearchOrder() const{ if (root) root->verifySearchOrder(); }
+	/*DONE HERE*/void verifyParentProperty() const{ if (root) root->verifyParentProperty(); }
+	/*DONE HERE*/void printPreorder(ostream &os = cout) const{if (root) root->printPreorder(os);}
 
-	/*ID=2 */void insert(const Base &item);
-	/*ID=3 */void remove(const Base &item);
-	/*ID=4 */bool find(const Base &item);
-	/*ID=5 */void splayToRoot(SplayNode<Base>* ptr);
-	
-	/*ID=6 DONE*/void printLevelOrder(ostream &os = cout) const;
-	/*ID=7 DONE*/void printPreorder(ostream &os = cout) const{if (root) root->printPreorder(os);}
-
-    void verifySearchOrder() const{ if (root) root->verifySearchOrder(); }
-    void verifyParentProperty() const{ if (root) root->verifyParentProperty(); }
+	/*ID=0 DONE*/void insert(const Base &item);
+	/*ID=1 */void remove(const Base &item);
+	/*ID=2 DONE*/bool find(const Base &item);
+	/*ID=3 DONE*/void splayToRoot(SplayNode<Base>* ptr);
+	/*ID=4 DONE*/void printLevelOrder(ostream &os = cout) const;
 
 protected:
 	SplayNode<Base> *root;
 };
-
-
-
-
-
 
 #endif
